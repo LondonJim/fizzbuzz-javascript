85c99c025b9ff5aa1b295f577a77164df030fc0f
James Sutherland <jrsutherland78@googlemail.com>
2018-09-04 13:01:55 +0100

    create prototype

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,13 +1,22 @@
-function Fizzbuzz() {
-  this.play = function(number) {
-    if (number % 5 === 0 && number % 3 === 0 ) {
-      return 'fizzbuzz';
-    } else if (number % 5 === 0) {
-      return 'buzz';
-    } else if (number % 3 === 0) {
-      return 'fizz';
-    } else {
-      return number;
-    }
+function FizzBuzz() {};
+
+FizzBuzz.prototype.play = function(number) {
+  if (number % 5 === 0 && number % 3 === 0 ) {
+    return 'fizzbuzz';
+  } else if (number % 5 === 0) {
+    return 'buzz';
+  } else if (number % 3 === 0) {
+    return 'fizz';
+  } else {
+    return number;
   }
+};
+
+/*
+var fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
 }
+
+*/
